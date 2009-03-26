4a8b42cb19e9eb9822823bb6283b665dfd175427
rev087 <rev087@e5ab5469-b565-0410-a8fb-e76fe78a19bf>
2009-03-26 02:40:32 +0000

    - Changes in the framework website - View class - Autoloading

--- controllers/class.Main.php
+++ controllers/class.Main.php
@@ -1,17 +1,25 @@
 <?php
 	class Main extends Controller {
 		
+		var $default_template = "template";
+		var $auto_render = TRUE;
+		
+		function __construct() {
+			$this->template = new View('template');
+		}
+		
 		function index($page="") {
-			require_once(Vault::getPath('templates').'\default.php');
+			$this->template->content = new View('home');
+			$this->template->render();
+			
 		}
 		
 		function about() {
-			print '<h1>About</h1>';
 		}
 		
 		function notfound() {
-			require_once(Vault::getPath('templates').'\default.php');
-			print 'NOT  FOUND!';
+			$this->template->content = "404 - Not found!";
+			$this->template->render();
 		}
 		
 	}
---
+++ system/Vault.View.php
@@ -0,0 +1,44 @@
+<?php
+	
+	class View {
+		private $view;
+		private $vars = array();
+		
+		function __construct($view) {
+			$this->view = $view;
+		}
+		
+		function __tostring() {
+			return $this->_getOutput();
+		}
+		
+		function __set($var, $value) {
+			$this->vars[$var] = $value;
+		}
+		
+		private function _getOutput() {
+			//begin intercepting the output buffer the buffer
+			ob_start();
+			
+			//set all template variables
+			foreach($this->vars as $var =>$val) {
+				$$var = $val;
+			}
+			
+			//require the view
+			require_once(Vault::getResourcePath('view', $this->view));
+			
+			//unset all template variables
+			foreach($this->vars as $var =>$val) {
+				unset($$var);
+			}
+			
+			//return the output and close the buffer
+			return ob_get_clean();
+		}
+		
+		function render() {
+			print $this->_getOutput();
+		}
+	}
+?>
