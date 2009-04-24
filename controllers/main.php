3b26d5a77d8bc95fc7ee004f7365cfd675909479
Bruno Daniel <bruno@fds.me>
2009-04-24 10:09:35 +0000

    Just a small fix to update the presentation webpage to the framework changes

---
+++ controllers/main.php
@@ -0,0 +1,26 @@
+<?php
+	class Main_controller extends Controller {
+		
+		var $default_template = "template";
+		var $auto_render = TRUE;
+		
+		function __construct() {
+			$this->template = new View('template');
+		}
+		
+		function index($page="") {
+			$this->template->content = new View('home');
+			$this->template->render();
+			
+		}
+		
+		function about() {
+		}
+		
+		function notfound() {
+			$this->template->content = "404 - Not found!";
+			$this->template->render();
+		}
+		
+	}
+?>
