d976ea05955c34f8e849d0934f79c50395d7ba65
rev087 <rev087@e5ab5469-b565-0410-a8fb-e76fe78a19bf>
2009-04-25 11:13:16 +0000

    Implemented the Assets system controller

---
+++ system/controllers/assets.php
@@ -0,0 +1,13 @@
+<?php
+	class Assets_controller extends Controller{
+		function __construct() {
+			parent::__construct();
+		}
+		
+		function index($type, $file) {
+			$target = Vault::$app_root_uri . Vault::getSetting('path', 'assets') .  "/{$type}/{$file}";
+			header("location: $target");
+		}
+		
+	}
+?>
