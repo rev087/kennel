849d28e147980b2e69dfa5f0e3080951d4101b4e
Bruno Daniel <bruno@fds.me>
2009-05-02 11:51:36 +0000

    * Fixes on the Assets system controller reflecting changes in the core methods * Much of the functionality previously in the resolveRequest is now in the controllerAction core method * url function now prints by default * removed debugRequest core method * removed requireResource core method * removed getResourcePath core method

--- system/controllers/assets.php
+++ system/controllers/assets.php
@@ -1,11 +1,28 @@
 <?php
 	class Assets_controller extends Controller{
-		function __construct() {
+		
+		function __construct()
+		{
 			parent::__construct();
 		}
 		
-		function index($type, $file) {
-			$target = Vault::$app_root_uri . Vault::getSetting('path', 'assets') .  "/{$type}/{$file}";
+		function index($type)
+		{
+		}
+		
+		function css($file)
+		{
+			header("Content-type: text/css");
+			$target = Vault::$app_root_path . Vault::getSetting('path', 'assets') . "/css/{$file}";
+			include($target);
+		}
+		
+		function notfound($type) {
+			$args = func_get_args();
+			$file = array_slice($args, 1);
+			$file = join('/', $file);
+			
+			$target = Vault::$app_root_uri . Vault::getSetting('path', 'assets') . "/{$type}/{$file}";
 			header("location: $target");
 		}
 		
