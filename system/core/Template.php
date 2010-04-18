90ed2d12004c1fd1fbc0c749f0e9002985a85db1
Bruno Daniel <bruno@fds.me>
2010-04-18 15:19:02 +0000

    * Dropping 't' helper, instead using: * Template core library. Works exactly like a View (it extends the View core library), but has these additional features: * Built-in basic HTML structure (html, head, body) * Dynamic page titles (current page title appended with the application title) * Template::css() to add stylesheets * Template::js() to add styles * Template::meta() to add meta tags * All the functionality above available from the children views via $template variable

--- system/core/Template.php
+++ system/core/Template.php
@@ -1,44 +1,126 @@
 <?php
 	
 	/**
-		*  TODO; Don't use this library yet.
+		*  TODO i18n
 		*/
-	
-	class Template
+	class Template extends View
 	{
-		private static $view;
-		private static $stylesheets = array();
-		private static $scripts = array();
+		// Static variables
+		static $DOCTYPE_DECLARATIONS = array(
+			'XHTML 1.0 Transitional' => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
+			'XHTML 1.0 Strict' => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+			'HTML 5' => '<!doctype html>'
+		);
 		
-		function __construct($view=null)
+		// Variables
+		var $doctype = 'XHTML 1.0 Transitional';
+		var $lang = 'pt-br';
+		var $dir = 'ltr';
+		var $favicon = null;
+		
+		// Structure
+		private $_html;
+		private $_head;
+		private $_body;
+		
+		// Meta
+		private $_meta = array();
+		
+		// Resources
+		private $_stylesheets = array();
+		private $_scripts = array();
+		
+		// Template View
+		private $_template;
+		
+		function __construct($template)
 		{
-			if(!isset(self::$view)) self::setView($view);
+			$this->_template = new View($template, $this);
+			$this->_template->template = $this;
 		}
 		
-		function setView($view)
+		// Ovewriting normal View behavior
+		//////////////////////////////////
+		
+		private function _getOutput()
 		{
-			self::$view = new View($view);
+			// Template View
+			$templateView = XML::text($this->_template->__toString());
+			
+			// <html>
+			$this->_html = XML::element('html');
+			$this->_html->dir =$this->dir;
+			$this->_html->lang = $this->lang;
+			
+			// <head>
+			$this->_head = XML::element('head', $this->_html);
+			
+			// <title>
+			$title = XML::element('title', $this->_head);
+			$title->setValue($this->getTitle());
+			
+			// favicon
+			if ($this->favicon)
+				$this->_head->adopt(html::favicon($this->favicon));
+			
+			// content type
+			$this->_head->adopt(html::meta('Content-Type', 'text/html; charset=UTF-8'));
+			
+			// <meta>
+			foreach ($this->_meta as $meta)
+				$this->_head->adopt(html::meta($meta['name'], $meta['content']));
+			
+			// <style>
+			$this->_head->adopt(html::css($this->_stylesheets));
+			
+			// <script>
+			$this->_head->adopt(html::js($this->_scripts));
+			
+			// <body>
+			$this->_body = XML::element('body', $this->_html);
+			
+			// Inject the Template View
+			$this->_body->adopt($templateView);
+			
+			// Return the whole shebang
+			return self::$DOCTYPE_DECLARATIONS[$this->doctype] . "\n" . $this->_html->output();
 		}
 		
-		function addStylesheet($filename)
+		function render()
 		{
-			if(array_search($filename, self::$stylesheets) === null)
-				self::$stylesheets[] = $filename;
+			print $this->_getOutput();
 		}
 		
-		function addJavascript($filename)
+		// Aditional utility methods
+		////////////////////////////
+		
+		private function getTitle()
 		{
-			if(array_search($filename, self::$scripts) === null)
-				self::$scripts[] = $filename;
+			if ($this->title && Kennel::getSetting('application', 'app_title'))
+				return $this->title . ' | ' . Kennel::getSetting('application', 'app_title');
+			elseif (!$this->title && Kennel::getSetting('application', 'app_title'))
+				return Kennel::getSetting('application', 'app_title');
+			elseif ($this->title && !Kennel::getSetting('application', 'app_title'))
+				return $this->title;
+			else
+				return 'Untitled';
 		}
 		
-		function header()
+		function meta($name, $content)
 		{
-			foreach (self::$stylesheets as $filename)
-				print html::css($filename);
-			foreach (self::$scripts as $filename)
-				print html::js($filename);
+			$this->_meta[] = array('name'=>$name, 'content'=>$content);
 		}
 		
+		function css()
+		{
+			$arguments = func_get_args();
+			$this->_stylesheets = array_merge($this->_stylesheets, $arguments);
+		}
+		
+		function js()
+		{
+			$arguments = func_get_args();
+			$this->_scripts = array_merge($this->_scripts, $arguments);
+		}
 	}
 ?>
