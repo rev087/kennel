a99c630e87029dad61428d778d65f75720acfdb5
rev087 <rev087@e5ab5469-b565-0410-a8fb-e76fe78a19bf>
2010-08-03 15:56:41 +0000

    * Small fixes for the Image core library

--- system/core/Image.php
+++ system/core/Image.php
@@ -10,7 +10,7 @@
 		function __construct($image) {
 			if (is_string($image))
 			{
-				if (!is_file($image)) return;
+				if (!is_file($image)) return false;
 				$this->path = pathinfo($image);
 				$imageSize = getimagesize($image);
 				$this->width = $imageSize[0];
@@ -42,13 +42,42 @@
 			imagedestroy($this->image);
 		}
 		
-		function output() {
-			header("Content-Type: image/jpeg");
+		function output($debug=false) {
+			if (!$debug) header("Content-Type: image/jpeg");
 			imagejpeg($this->image, null, 95);
+		}
+		
+		function fit($width, $height, $bgColor=array(255, 0, 0))
+		{
+			// Does exactly the same thing as Image::thumb, but fits the whole original image in the destination image instead of croping
+			//TODO: set background color
+			if (!$this->image) return;
+			$ratio = $this->width / $this->height;
+		 
+			if ($width / $height < $ratio) {
+				 $new_height = $width / $ratio;
+				 $new_width = $width;
+			} else {
+				 $new_width = $height * $ratio;
+				 $new_height = $height;
+			}
+		 
+			$x_mid = $new_width / 2;  //horizontal middle
+			$y_mid = $new_height / 2; //vertical middle
+		 
+			$process = imagecreatetruecolor(round($new_width), round($new_height));
+		 
+			imagecopyresampled($process, $this->image, 0, 0, 0, 0, $new_width, $new_height, $this->width, $this->height);
+			$fit = imagecreatetruecolor($width, $height);
+			imagecopyresampled($fit, $process, 0, 0, ($x_mid-($width/2)), ($y_mid-($height/2)), $width, $height, $width, $height);
+			
+			imagedestroy($process);
+			return $this->image = $fit;
 		}
 		
 		function thumb($width, $height)
 		{
+			// Picks the best fit from the original to the destination image, cropping when necessary
 			if (!$this->image) return;
 			$ratio = $this->width / $this->height;
 		 
@@ -106,7 +135,6 @@
 			$this->height = $new_h;
 			
 			$this->image = $resized;
-			imagedestroy($resized);
 			return $this->image;
 		}
 		
