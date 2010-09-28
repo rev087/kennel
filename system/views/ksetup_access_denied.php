b44b44e7902662c0e0a0cf11eebd0eca22bcd7d2
rev087 <rev087@e5ab5469-b565-0410-a8fb-e76fe78a19bf>
2010-09-28 18:31:30 +0000

    * Added login from the KSetup interface

--- system/views/ksetup_access_denied.php
+++ system/views/ksetup_access_denied.php
@@ -1,3 +1,12 @@
 <h1>Access Denied</h1>
 
 <p class="msg error">ERROR: You do not have permission to access this feature</p>
+
+<form method="post" action="<?php print url('ksetup/login'); ?>">
+	<fieldset>
+		<legend>Login</legend>
+		<label>user <input type="text" name="username" autofocus /></label>
+		<label>pass <input type="password" name="password" /></label>
+		<input type="submit" />
+	</fieldset>
+</form>
