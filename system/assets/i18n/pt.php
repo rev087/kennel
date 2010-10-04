192724db76a9026da510c0158a254893f173d8fe
Bruno Daniel <bruno@fds.me>
2010-10-04 22:21:44 +0000

    * Adding the i18n strings again (oops)

---
+++ system/assets/i18n/pt.php
@@ -0,0 +1,9 @@
+<?php
+	$data = array(
+		'Please type a valid e-mail address.'=>'Por favor digite um endereço de e-mail válido.',
+		'This field accepts numbers only.'=>'Este campo aceita apenas números.',
+		'Please type a valid amount.'=>'Por favor digite um valor válido.',
+		'Please type a valid value for this field.'=>'Por favor entre um valor válido para este campo.',
+		'This field is required.'=>'Este campo é obrigatório'
+	);
+?>
