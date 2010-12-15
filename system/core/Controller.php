024b130f5e42e9669218fbf2222905352f39ceab
rev087 <rev087@e5ab5469-b565-0410-a8fb-e76fe78a19bf>
2010-12-15 12:31:43 +0000

    * function __construct() for Controller class (empty...just to avoid errors when trying parent::__constructor() from child classes)

--- system/core/Controller.php
+++ system/core/Controller.php
@@ -4,11 +4,20 @@
 	{
 		static $_instances = array();
 		
+		function __construct()
+		{
+		}
+		
 		static function getInstance($descendent)
 		{
 			if(!array_key_exists($descendent, self::$_instances))
 				self::$_instances[$descendent] = new $descendent;
 			return self::$_instances[$descendent];
+		}
+		
+		function notfound()
+		{
+			 header("Status: 404 Not Found");
 		}
 		
 	}
