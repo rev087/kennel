e594efd809290842e7e4a05343135e2c546a0300
Bruno Daniel <bruno@vault7.com>
2012-06-06 19:28:37 -0300

    flash (as in session based flash messages) helper

--- system/helpers/flash.php
+++ system/helpers/flash.php
@@ -1,27 +1,38 @@
 <?php
-	class flash
-	{
-		public static function embed($filename, $width=null, $height=null, $wmode='opaque')
-		{
-			$path = assets::flash($filename);
-			
-			return '<object classid=˜clsid:D27CDB6E-AE6D-11cf-96B8-444553540000˜ width="'.$width.'" height="'.$height.' id="banner" name="player1">
-			   <param name="movie" value="player.swf">
-			   <param name="allowfullscreen" value="true">
-			   <param name="allowscriptaccess" value="always">
-			   <param name="flashvars" value="file=playlist.xml">
-			   <param name="wmode" value="'.$wmode.'">
-			   <embed id="banner"
-			          name="banner"
-			          src="'.$path.'"
-			          width="'.$width.'"
-			          height="'.$height.'"
-			          wmode="'.$wmode.'"
-			          allowscriptaccess="always"
-			          allowfullscreen="true"
-			          flashvars="file=playlist.xml"
-			   />
-			</object>';
-		}
-	}
+  class flash
+  {
+    static function set($slot, $message)
+    {
+			if(!session_id()) session_start();
+      $app_id = Kennel::getSetting('application', 'id');
+      
+      $_SESSION["{$app_id}-flash"][$slot] = $message;
+    }
+    
+    static function get($slot)
+    {
+			if(!session_id()) session_start();
+      $app_id = Kennel::getSetting('application', 'id');
+      
+      if ( isset($_SESSION["{$app_id}-flash"][$slot]) ) {
+        $message = $_SESSION["{$app_id}-flash"][$slot];
+        unset($_SESSION["{$app_id}-flash"][$slot]);
+        return $message;
+      } else {
+        return null;
+      }
+    }
+    
+    static function has($slot)
+    {
+			if(!session_id()) session_start();
+      $app_id = Kennel::getSetting('application', 'id');
+      
+      if ( isset($_SESSION["{$app_id}-flash"][$slot]) && count($_SESSION["{$app_id}-flash"][$slot]) > 0 )
+        return true;
+      else
+        return false;
+    }
+    
+  }
 ?>
