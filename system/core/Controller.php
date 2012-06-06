a3276eaa1575b1807cfec336d68039d5a850d779
Bruno Daniel <bruno@vault7.com>
2012-06-06 19:34:26 -0300

    Base Controller now renders the 404.php view in the default notfound() method; added the 'redirect' private method to Controller

--- system/core/Controller.php
+++ system/core/Controller.php
@@ -18,6 +18,26 @@
 		function notfound()
 		{
 			 header("Status: 404 Not Found");
+			 
+			 $view = new View('404');
+			 $view->render();
+		}
+		
+		private function redirect($location, $status=303)
+		{
+		  switch ($status)
+		  {
+		    case 301: header("Status: 301 Moved Permanently");
+		    case 303: header("Status: 303 See Other");
+		  }
+		  
+		  $url = parse_url($location);
+		  if ( !isset($scheme) )
+  		  header("Location: " . url($location));
+      else
+  		  header("Location: " . $location);
+  		
+  		exit(0);
 		}
 		
 	}
